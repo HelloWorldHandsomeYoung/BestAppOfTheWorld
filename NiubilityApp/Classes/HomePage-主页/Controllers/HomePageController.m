e74c83f0a20706f25c138c97aff81fe5c964eeb9
HelloWorldHandsomeYoung <361447533@qq.com>
2016-03-01 14:08:09 +0800

    基本框架

---
+++ NiubilityApp/Classes/HomePage-主页/Controllers/HomePageController.m
@@ -0,0 +1,113 @@
+//
+//  HomePageController.m
+//  NiubilityApp
+//
+//  Created by 吕阳 on 16/3/1.
+//  Copyright © 2016年 DeveloperYoung. All rights reserved.
+//
+
+#import "HomePageController.h"
+#import "HomePageViewCell.h"
+
+@interface HomePageController ()
+
+@end
+static  NSString *cellID = @"HomeCell";
+@implementation HomePageController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self.tableView registerNib:[UINib nibWithNibName:@"HomePageViewCell" bundle:nil] forCellReuseIdentifier:cellID];
+    
+    [self setupNavigation];
+}
+
+- (void)setupNavigation
+{
+    UIView *view = [[UIView alloc]initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 64)];
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    button.width = 50;
+    button.height = 64;
+    button.center = view.center;
+    [button setTitle:@"测试" forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(test) forControlEvents:UIControlEventTouchUpInside];
+    [view addSubview:button];
+    self.navigationItem.titleView = view;
+}
+- (void)test
+{
+    [UIView animateWithDuration:1 animations:^{
+        self.navigationController.navigationBar.frame = CGRectMake(0, -64, [UIScreen mainScreen].bounds.size.width, 64);
+    }];
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 100;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    HomePageViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID forIndexPath:indexPath];
+    
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
