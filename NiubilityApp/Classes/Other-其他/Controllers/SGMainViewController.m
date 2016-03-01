9a403e8d72afa539218688a9a36d27f431e36871
HUI24 <425822521@qq.com>
2016-03-01 16:30:21 +0800

    hh

---
+++ NiubilityApp/Classes/Other-其他/Controllers/SGMainViewController.m
@@ -0,0 +1,37 @@
+//
+//  SGMainViewController.m
+//  NiubilityApp
+//
+//  Created by 王辉 on 16/3/1.
+//  Copyright © 2016年 DeveloperYoung. All rights reserved.
+//
+
+#import "SGMainViewController.h"
+
+@interface SGMainViewController ()
+
+@end
+
+@implementation SGMainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
